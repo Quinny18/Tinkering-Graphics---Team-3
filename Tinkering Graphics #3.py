a577a0230afa2f78775be25fd52fa72f757fb31c
LMR589281 <46445319+LMR589281@users.noreply.github.com>
2019-10-21 21:08:21 +0100

    Add files via upload
    tinker graphics #3 version 4
    +allows user to keep to discard monsters
    +put everything into functions
    +added comments

--- Tinkering Graphics #3.py
+++ Tinkering Graphics #3.py
@@ -1,67 +1,76 @@
 import random
 import pygame
-import time
+
+
+def load_assets():  # this function loads all the assets of the monster parts
+    heads = ["Assets\Heads\HumanHead.png", "Assets\Heads\LizardHead.png", "Assets\Heads\MonkeyHead.png",
+             "Assets\Heads\ToucanHead.png"]
+    # horns=["Assets\Horns\BigHorn.png","Assets\Horns\Little Horn.png"]
+    wings = ["Assets\Wings\BatWing.png", "Assets\Wings\ParrotWing.png", "Assets\Wings\ToucanWing.png"]
+    arms = ["Assets\Arms\HumanArm.png", "Assets\Arms\LizardArm.png", "Assets\Arms\LobsterArm.png",
+            "Assets\Arms\MonkeyArm.png"]
+    torsos = ["Assets\Torsos\HumanTorso.png", "Assets\Torsos\LizardTorso.png", "Assets\Torsos\MonkeyTorso.png",
+              "Assets\Torsos\ToucanTorso.png"]
+    legs = ["Assets\Legs\HumanLeg.png", "Assets\Legs\LizardLeg.png", "Assets\Legs\MonkeyLeg.png",
+            "Assets\Legs\ToucanLeg.png"]
+    monster_parts = heads + wings + arms + torsos + legs
+    return monster_parts
+
+
+def random_number_generator():  # this function generates random numbers
+    head = random.randint(0, 3)
+    wing = random.randint(4, 6)
+    arm = random.randint(7, 10)
+    torso = random.randint(11, 14)
+    leg = random.randint(15, 18)
+    random_numbers = [head, wing, arm, torso, leg]
+    return random_numbers
+
+
+def print_monster(monster, number):  # this function loads all the assets for the monster into pygame and lines them up
+    head, wing, arm, torso, leg = number[0], number[1], number[2], number[3], number[4]
+    main_window.blit(pygame.image.load(monster[wing]), (60, 30))  # wing
+    main_window.blit(pygame.image.load(monster[arm]), (20, 70))  # arm
+    main_window.blit(pygame.image.load(monster[leg]), (25, 120))  # leg, out or range?
+    main_window.blit(pygame.image.load(monster[torso]), (25, 60))  # torso
+    main_window.blit(pygame.image.load(monster[leg]), (55, 120))  # leg
+    main_window.blit(pygame.image.load(monster[arm]), (55, 70))  # arm
+    main_window.blit(pygame.image.load(monster[head]), (0, 40))  # head
+    # main_window.blit(load_Horn, (40, 20))
+
+
+def keep_monster():  # this function allows the user to keep their monster or get a new one
+    loop = True
+    while loop == True:
+        keep = input("Do you want to keep the current monster?")
+        if keep == "yes":
+            print('Your monster has been saved as "Monster.png"')
+            pygame.image.save(main_window, "Monster.png")
+            input("Press enter to end the program")
+            running = False
+            loop = False
+        elif keep == "no":
+            print("Loading next monster")
+            loop = False
+        else:
+            print('Please enter "yes" or "no"')
+
 
 pygame.init()
-main_window = pygame.display.set_mode((150,220))
+main_window = pygame.display.set_mode((150, 220))
 running = True
 
-heads=[r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Heads\HumanHead.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Heads\LizardHead.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Heads\MonkeyHead.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Heads\ToucanHead.png"]
-
-horns=[r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Horns\BigHorn.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Horns\Little Horn.png"]
-
-wings=[r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Wings\BatWing.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Wings\ParrotWing.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Wings\ToucanWing.png"]
-
-arms=[r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Arms\HumanArm.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Arms\LizardArm.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Arms\LobsterArm.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Arms\MonkeyArm.png"]
-
-torsos=[r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Torsos\HumanTorso.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Torsos\LizardTorso.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Torsos\MonkeyTorso.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Torsos\ToucanTorso.png"]
-
-legs=[r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Legs\HumanLeg.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Legs\LizardLeg.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Legs\MonkeyLeg.png",
-r"C:\Users\isaac\Desktop\Tinkering-Graphics---Team-3\Assets\Legs\ToucanLeg.png"]
-
+monster_parts = load_assets()
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-
-    #images = [my_surface,my_surface2]
-    #location = [(0, 60) ,(50, 60)]
-    #length = len(images)
-
-    main_window.fill((255,255,255))
-    load_Arm = pygame.image.load (random.choice(arms))
-    load_Head = pygame.image.load(random.choice(heads))
-    load_Leg = pygame.image.load (random.choice(legs))
-    load_Torso = pygame.image.load(random.choice(torsos))
-    load_Wing = pygame.image.load(random.choice(wings))
-    load_Horn = pygame.image.load(random.choice(horns))
-
-    main_window.blit(load_Wing, (60, 30))
-    main_window.blit(load_Arm, (20, 70))
-    main_window.blit(load_Leg, (25, 120))
-    main_window.blit(load_Torso, (25, 60))
-    main_window.blit(load_Leg, (55, 120))
-    main_window.blit(load_Arm, (55, 70))
-    main_window.blit(load_Head, (0, 40))
-    #main_window.blit(load_Horn, (40, 20))
-
+    main_window.fill((255, 255, 255))
+    random_numbers = random_number_generator()
+    print_monster(monster_parts, random_numbers)
     pygame.display.update()
-    time.sleep(2)
-pygame.image.save(main_window,"Monster.png")
+    keep_monster()
+
 pygame.quit()
