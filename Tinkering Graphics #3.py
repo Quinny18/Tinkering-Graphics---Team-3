27e9d762345c27750af94ea65dab94b3ede19974
LMR589281 <46445319+LMR589281@users.noreply.github.com>
2019-10-23 20:53:01 +0100

    Add files via upload
    fixed bug that causes the program not to close and added more comments to contract 3
    added user interface and added more comments to contract 4

--- Tinkering Graphics #3.py
+++ Tinkering Graphics #3.py
@@ -4,17 +4,17 @@ import pygame
 
 def load_assets():  # this function loads all the assets of the monster parts
     heads = ["Assets\Heads\HumanHead.png", "Assets\Heads\LizardHead.png", "Assets\Heads\MonkeyHead.png",
-             "Assets\Heads\ToucanHead.png"]
-    # horns=["Assets\Horns\BigHorn.png","Assets\Horns\Little Horn.png"]
+             "Assets\Heads\ToucanHead.png"]    # all the head assets are stored in an array
     wings = ["Assets\Wings\BatWing.png", "Assets\Wings\ParrotWing.png", "Assets\Wings\ToucanWing.png"]
+    # all the wing assets are stored in an array
     arms = ["Assets\Arms\HumanArm.png", "Assets\Arms\LizardArm.png", "Assets\Arms\LobsterArm.png",
-            "Assets\Arms\MonkeyArm.png"]
+            "Assets\Arms\MonkeyArm.png"]    # all the arm assets are stored in an array
     torsos = ["Assets\Torsos\HumanTorso.png", "Assets\Torsos\LizardTorso.png", "Assets\Torsos\MonkeyTorso.png",
-              "Assets\Torsos\ToucanTorso.png"]
+              "Assets\Torsos\ToucanTorso.png"]    # all the torso assets are stored in an array
     legs = ["Assets\Legs\HumanLeg.png", "Assets\Legs\LizardLeg.png", "Assets\Legs\MonkeyLeg.png",
-            "Assets\Legs\ToucanLeg.png"]
-    monster_parts = heads + wings + arms + torsos + legs
-    return monster_parts
+            "Assets\Legs\ToucanLeg.png"]    # all the leg assets are stored in an array
+    monster_parts = heads + wings + arms + torsos + legs # all arrays are combined into one large array
+    return monster_parts  # this array it returned to the main program
 
 
 def random_number_generator():  # this function generates random numbers
@@ -24,54 +24,53 @@ def random_number_generator():  # this function generates random numbers
     torso = random.randint(11, 14)
     leg = random.randint(15, 18)
     random_numbers = [head, wing, arm, torso, leg]
-    return random_numbers
+    return random_numbers  # the random numbers are put into an array then returned to the main program
 
 
 def print_monster(monster, number):  # this function loads all the assets for the monster into pygame and lines them up
     head, wing, arm, torso, leg = number[0], number[1], number[2], number[3], number[4]
-    main_window.blit(pygame.image.load(monster[wing]), (60, 30))  # wing
-    main_window.blit(pygame.image.load(monster[arm]), (20, 70))  # arm
-    main_window.blit(pygame.image.load(monster[leg]), (25, 120))  # leg, out or range?
-    main_window.blit(pygame.image.load(monster[torso]), (25, 60))  # torso
-    main_window.blit(pygame.image.load(monster[leg]), (55, 120))  # leg
-    main_window.blit(pygame.image.load(monster[arm]), (55, 70))  # arm
-    main_window.blit(pygame.image.load(monster[head]), (0, 40))  # head
-    # main_window.blit(load_Horn, (40, 20))
+    main_window.blit(pygame.image.load(monster[wing]), (60, 30))  # loads the wings
+    main_window.blit(pygame.image.load(monster[arm]), (20, 70))  # loads the left arm
+    main_window.blit(pygame.image.load(monster[leg]), (25, 120))  # loads the left leg
+    main_window.blit(pygame.image.load(monster[torso]), (25, 60))  # loads the torso
+    main_window.blit(pygame.image.load(monster[leg]), (55, 120))  # loads the right leg
+    main_window.blit(pygame.image.load(monster[arm]), (55, 70))  # loads the right arm
+    main_window.blit(pygame.image.load(monster[head]), (0, 40))  # loads the head
 
 
 def keep_monster():  # this function allows the user to keep their monster or get a new one
     loop, running = True, True
-    while loop == True:
+    while loop == True:  # makes the function loop if the player does not input yes or no
         keep = input("Do you want to keep the current monster?")
-        if keep == "yes":
+        if keep == "yes":  # ask the user if they want to keep the generated monster
             print('Your monster has been saved as "Monster.png"')
-            pygame.image.save(main_window, "Monster.png")
+            pygame.image.save(main_window, "Monster.png")  # saves the monster as a png file
             input("Press enter to end the program")
             running = False
             loop = False
         elif keep == "no":
             print("Loading next monster")
-            loop = False
+            loop = False  # ends the function and starts the generation of a new monster
         else:
             print('Please enter "yes" or "no"')
     return running
 
-
+# the below code is the main program
 pygame.init()
-main_window = pygame.display.set_mode((150, 220))
+main_window = pygame.display.set_mode((150, 220))  # declares the size of the window
 running = True
 
-monster_parts = load_assets()
-
+monster_parts = load_assets()  # loads the assets
+# basic pygame set up to keep the window up
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     main_window.fill((255, 255, 255))
-    random_numbers = random_number_generator()
-    print_monster(monster_parts, random_numbers)
-    pygame.display.update()
-    running = keep_monster()
+    random_numbers = random_number_generator()  # generates the random numbers
+    print_monster(monster_parts, random_numbers)  # prints the monster to the pygame window
+    pygame.display.update()  # updates the window with the new monster
+    running = keep_monster()  # ask user if they want to keep the monster
 
 pygame.quit()
--- Tinkering Graphics #4.py
+++ Tinkering Graphics #4.py
@@ -2,56 +2,103 @@ import math
 import random
 import pygame
 
+
+def define_colours():  # defines some colours used in the assets
+    green = (173, 243, 77)
+    gray = (211, 211, 211)
+    red = (204, 27, 0)
+    beige = (250, 221, 182)
+    lower_green = (173, 125, 77)
+    lower_gray = (161, 161, 161)
+    lower_red = (154, 27, 0)
+    lower_beige = (230, 201, 162)
+    colours = [green, gray, red, beige]  # puts the colours into an array
+    lighter_colours = [lower_green, lower_gray, lower_red, lower_beige]  # puts the light colours into an array
+    return colours, lighter_colours  # returns the light and normal colour arrays
+
+
+def pick_colour():  # allows the user to pick what colours will be changed
+    loop = False
+    while loop == False:
+        print("Pick the colour you want to remove and then the colour you want to replace it with")
+        remove_colour = str(input("1)Green\n2)Gray\n3)Red\n4)Beige\n"))
+        replace_colour = str(input("1)Green\n2)Gray\n3)Red\n4)Beige\n"))
+        if remove_colour == "1" or remove_colour == "2" or remove_colour == "3" or remove_colour == "4":
+        # checks to make sure user has entered 1,2,3 or 4 for the first number
+            if replace_colour == "1" or replace_colour == "2" or replace_colour == "3" or replace_colour == "4":
+            # checks to make sure user has entered 1,2,3 or 4 for the second number
+                if replace_colour == remove_colour:
+                # checks to make sure the user has not picked the two same colours
+                    print("Can't Have Two Colours Be The Same")
+                else:
+                    # convertes the user input to integers so they can be used in the colour array
+                    remove_colour = int(remove_colour) - 1
+                    replace_colour = int(remove_colour) - 1
+                    loop = True
+        else:
+            print("Please Enter 1,2,3 or 4")
+    return remove_colour, replace_colour  # returns the numbers to the main program
+
+
+def colour_distance(colour1, colour2):  # checks how similar two colours are
+    answer = math.sqrt((colour1[0] - colour2[0]) ** 2 + (colour1[1] - colour2[1]) ** 2 + (colour1[2] - colour2[2]) ** 2)
+    return answer  # returns the answer
+
+
+def unshade(colour1, colour2, tolerance, surface=pygame.Surface((1, 1))):
+# takes a colour and converts all similar shades to the same colour
+    pixel = pygame.Color(0, 0, 0)
+    for x in range(surface.get_width()):
+        # loops through all x values
+        for y in range(surface.get_width()):
+            # loops through all y values
+            pixel = surface.get_at((x, y))  # gets the location of the pixel
+            if colour_distance(colour1, colour2) < tolerance:
+                # changes colour if it is similar to the selected colour
+                surface.set_at((x, y), pygame.Color(211, 211, 211))
+
+
+
+def colour_to_colour(colours, remove_colour, replace_colour, surface=pygame.Surface((1, 1))):
+# change one colour to another
+    pixel = pygame.Color(0, 0, 0)
+    for x in range(surface.get_width()):
+        # loops through all x values
+        for y in range(surface.get_width()):
+            # loops through all y values
+            pixel = surface.get_at((x, y))
+            if pixel == colours[remove_colour]:
+                # checks to find any colour that will be removed
+                surface.set_at((x, y), colours[replace_colour])
+                # sets the pixel to the new colour
+    print("""Recolouring Monster
+...
+Recolouring Finished
+""")
+
+
 pygame.init()
-main_window = pygame.display.set_mode((150, 220))
+main_window = pygame.display.set_mode((150, 220))  # sets the size of the pygame window
 running = True
 
-# imports monster
+# imports the monster from contract 3
 image = pygame.image.load("Monster.png")
 
-green = (173, 243, 77)
-gray = (211, 211, 211)
-red = (204, 27, 0)
-beige = (250, 221, 182)
-lower_Green = (173, 125, 77)
-higher_Green = (173, 255, 77)
-
-
-def colour_distance(colour1, colour2):
-    answer = math.sqrt((colour1[0]-colour2[0])**2+(colour1[1]-colour2[1])**2+(colour1[2]-colour2[2])**2)
-    return answer
-
-def make_green(colour1, colour2, tolerance, surface=pygame.Surface((1, 1))):
-    pixel = pygame.Color(0, 0, 0)
-    for x in range(surface.get_width()):
-        for y in range(surface.get_width()):
-            pixel = surface.get_at((x, y))
-            if colour_distance(colour1, colour2) < tolerance:
-                surface.set_at((x, y), pygame.Color(211, 211, 211))
-
-# change colours function (green to blue)
-def green_to_gray(surface=pygame.Surface((1, 1))):
-# check for pixels that are green
-    pixel = pygame.Color(0, 0, 0)
-    for x in range(surface.get_width()):
-        for y in range(surface.get_width()):
-            pixel = surface.get_at((x, y))
-            if pixel == green:
-                surface.set_at((x, y), pygame.Color(211, 211, 211))
-
-
-make_green(higher_Green, lower_Green, 135, image)
-green_to_gray(image)
-
+colours, lighter_colours = define_colours()  # defines all the colours that can be changed
+remove_colour, replace_colour = pick_colour()  # lets user pick the colours they want to change
+unshade(colours[remove_colour], lighter_colours[remove_colour], 100, image)
+# takes the selected colour that will be removed and turns all similar shades to the same colour
+colour_to_colour(colours, remove_colour, replace_colour, image)  # replaces the old colour with the new colour
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    main_window.fill((255,255,255))
+    main_window.fill((255, 255, 255))  # fills the back ground of the pygame window as white
     main_window.blit(image, (0, 0))
-    pygame.display.update()
+    pygame.display.update()  # updates the pygame window
 
-    # save new image
+    # save the new image as a png
+    pygame.image.save(main_window, "Monster.png")
 pygame.quit()
