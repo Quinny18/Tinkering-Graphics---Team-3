1e8d24a6a560e907d48aaca80af1a0efd180adba
LMR589281 <46445319+LMR589281@users.noreply.github.com>
2019-10-24 16:00:31 +0100

    Add files via upload
    Added a simple user interface to contract 5

--- Tinkering Graphics #5.py
+++ Tinkering Graphics #5.py
@@ -1,14 +1,26 @@
 import pygame
+import time
 
-pygame.init()
-
-
-answer = int(input("What type of texture do you want to generate? Enter the corresponding number"
-                   "\n 1) Normal: "
-                   "\n 2) Protanopia: "
-                   "\n 3) Deuteranopia: "
-                   "\n 4) Tritanopia: "
-                   "\n Enter Here: "))
+def blindness(my_surface):
+    loop=True
+    while loop==True:
+        answer = int(input("What type of texture do you want to generate? Enter the corresponding number\n 1) Normal: \n 2) "
+                           "Protanopia: \n 3) Deuteranopia: \n 4) Tritanopia: \n Enter Here:" ))
+        if answer == 1:
+            normal_view(my_surface)
+            loop=False
+        elif answer == 2:
+            protanopia_view(my_surface)
+            loop=False
+        elif answer == 3:
+            deuteranopia_view(my_surface)
+            loop=False
+        elif answer == 4:
+            tritanopia_view(my_surface)
+            loop=False
+        else:
+            print("Please Enter 1,2,3 or 4")
+    return my_surface
 
 
 def normal_view(picture):
@@ -17,6 +29,7 @@ def normal_view(picture):
         for b in range(picture.get_height()):
             pixel = picture.get_at((a,b))
             picture.set_at((a, b), pygame.Color(pixel.r, pixel.g, pixel.b))
+    return my_surface
 
 
 def protanopia_view(picture):
@@ -25,6 +38,7 @@ def protanopia_view(picture):
         for b in range(picture.get_height()):
             pixel = picture.get_at((a, b))
             picture.set_at((a, b), pygame.Color(int(pixel.r * 0.5), int(pixel.g * 0.5), pixel.b))
+    return my_surface
 
 
 def deuteranopia_view(picture):
@@ -33,6 +47,7 @@ def deuteranopia_view(picture):
         for b in range(picture.get_height()):
             pixel = picture.get_at((a, b))
             picture.set_at((a, b), pygame.Color(int(pixel.r * 0.5), int(pixel.g * 0.2), pixel.b))
+    return my_surface
 
 
 def tritanopia_view(picture):
@@ -41,31 +56,13 @@ def tritanopia_view(picture):
         for b in range(picture.get_height()):
             pixel = picture.get_at((a, b))
             picture.set_at((a, b), pygame.Color(pixel.r, int(pixel.g * 0.2), int(pixel.b * 0.5)))
+    return my_surface
 
-
+pygame.init()
 main_window = pygame.display.set_mode((800,600))
 my_surface = pygame.image.load('test.png').convert()
-
-normal_view(my_surface)
-
-if answer == 1:
-    normal_view(my_surface)
-
-elif answer == 2:
-    protanopia_view(my_surface)
-
-elif answer == 3:
-    deuteranopia_view(my_surface)
-
-elif answer == 4:
-    tritanopia_view(my_surface)
-
-else:
-    print("Error, invalid input. Please try again")
-
+blindness(my_surface)
 running = True
-
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -74,5 +71,7 @@ while running:
     main_window.fill((255, 255, 255))
     main_window.blit(my_surface, (0, 0))
     pygame.display.update()
-
+    print("Converted Image")
+    time.sleep(10)
+    running=False
 pygame.quit()
